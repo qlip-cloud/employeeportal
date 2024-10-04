88bbe082ce3615c7e1e23ab793b55c9451e997ab
Ashley <ashleydefort@outlook.es>
2024-10-04 21:47:46 +0000

    feat(employee-portal): add multiple doctypes for portal functionalities - Added Announcement doctype - Added Performance Review doctype - Added Employee Benefit doctype

---
+++ employee_portal/employee_portal/doctype/help_desk/test_help_desk.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Ashley Mercado Defort and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestHelpDesk(unittest.TestCase):
+	pass
---
+++ employee_portal/employee_portal/doctype/performance_review/performance_review.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Ashley Mercado Defort and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class PerformanceReview(Document):
+	pass
