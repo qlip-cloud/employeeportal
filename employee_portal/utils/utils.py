e0a61cf4b34d57e7242e5d2c381cef82974724df
Ashley <ashleydefort@outlook.es>
2024-10-24 14:46:08 +0000

    style(my_benefits): update bootstrap classes for improved form layout and alignment

--- employee_portal/utils/utils.py
+++ employee_portal/utils/utils.py
@@ -4,7 +4,7 @@ def get_modules(context):
     {"name": "Inicio", "url": "/employee_portal", "icon": "home"},
     {"name": "Mis Datos", "url": "/employee_portal/my_profile", "icon": "user"},
     {"name": "Mis Beneficios", "url": "/employee_portal/my_benefits", "icon": "gift"},
-    {"name": "Mis Permisos/Ausencias", "url": "/employee_portal/my_leaves", "icon": "calendar-alt"},
+    {"name": "Mis Permihnsos/Ausencias", "url": "/employee_portal/my_leaves", "icon": "calendar-alt"},
     {"name": "Mis Eventos", "url": "/employee_portal/my_events", "icon": "calendar"},
     {"name": "Mis Evaluaciones", "url": "/employee_portal/my_evaluations", "icon": "star"},
     {"name": "Noticias", "url": "/employee_portal/news", "icon": "newspaper"},
