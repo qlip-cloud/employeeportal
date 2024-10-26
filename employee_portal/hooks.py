b2b7738d22776f5fd4ddea205f4741fc47323003
Ashley <ashleydefort@outlook.es>
2024-10-26 20:50:31 +0000

    feat(employee_portal): add AJAX navigation with loading message for module content

--- employee_portal/hooks.py
+++ employee_portal/hooks.py
@@ -126,9 +126,6 @@ app_license = "MIT"
 # Overriding Methods
 # ------------------------------
 #
-# override_whitelisted_methods = {
-# 	"frappe.desk.doctype.event.event.get_events": "employee_portal.event.get_events"
-# }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
