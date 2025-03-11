89e019d7569308338290de7f29bd738fc7ab1019
Ashley <ashleydefort@outlook.es>
2025-03-11 01:41:31 +0000

    feat(leave-application): add details view for leave requests

--- employee_portal/www/employee_portal/leave_application/details/index.py
+++ employee_portal/www/employee_portal/leave_application/details/index.py
@@ -4,6 +4,9 @@ from employee_portal.utils.utils import get_modules, get_employee_from_user
 def get_context(context):
   get_modules(context)
   get_employee_from_user(context)
-  leave_name = frappe.form_dict.get("name")
-  context.leave = frappe.get_doc("Leave Application", leave_name)
+  leave_id = frappe.form_dict.get("name")
+  if leave_id:
+    context.leave = frappe.get_doc("Leave Application", leave_id)
+  else:
+    context.leave = None
   return context
